731baae72bccbf325bc0bf10b3768d66fba4db57
DC-Jade <jadebetter8092@gmail.com>
2022-05-10 11:20:05 +0800

    2022-05-10 complete sliding window of 3_length_of_longet_substring.cpp

--- src/3_length_of_longet_substring.cpp
+++ src/3_length_of_longet_substring.cpp
@@ -2,25 +2,35 @@
 #include <assert.h>
 
 #include <string>
+#include <ctime>
+#include <algorithm>
+
 
 using std::string;
 
+
 int SolutionOne(const string &s);
 int SolutionTwo(const string &s);
+int SolutionThree(const string &s);
+int SolutionFour(const string &s);
+int SolutionFive(const string &s);
 int Solution(const string &s);
 
 int Solution(const string &s) {
-	int rand_int = rand() % 2;
+	srand(time(NULL));
+	int rand_int = rand() % 5;
 	switch (rand_int) {
 		case 0: return SolutionOne(s); break;
 		case 1: return SolutionTwo(s); break;
+		case 2: return SolutionThree(s); break;
+		case 3: return SolutionFour(s); break;
+		case 4: return SolutionFive(s); break;
 	}
 	return 0;
 }
 
 int SolutionOne(const string &s) {
 	int len = s.length();
-
 	int max_len = 0;
 	int cur_len = 0;
 	for (int i = 0; i < len; ++i) {
@@ -57,7 +67,7 @@ int SolutionTwo(const string &s) {
 			// }
 			std::size_t pos = s.find(s[j], i);
 			if (pos != string::npos && pos < j) {
-				printf("pos: %d\n", pos);
+				printf("pos: %ld\n", pos);
 				printf("i = %d j = %d\n", i, j);
 				break;
 			}
@@ -70,9 +80,78 @@ int SolutionTwo(const string &s) {
 	return max_len;
 }
 
-/* TODO */
-void SolutionThree(const string &s) {
-	
+/* sliding window */
+int SolutionThree(const string &s) {
+	int start = 0, end = 0, cur_len = 0, result = 0;
+	int len = (int)(s.size());
+	while (end < len) {
+		char tmp_char = s[end];
+		for (int i = start; i < end; ++i) {
+			if (tmp_char == s[i]) {
+				start = i + 1;
+				cur_len = end - start;
+				break;
+			}
+		}
+		++end;
+		++cur_len;
+		result = std::max(cur_len, result);
+	}
+	printf("SolutionThree: %d\n", result);
+	return result;
+}
+
+/* sliding window with find() */
+int SolutionFour(const string &s) {
+	int start = 0, end = 0, cur_len = 0, max_len = 0;
+	int str_len = s.length();
+	std::size_t pos = 0;
+	for (end = 0; end < str_len; ++end) {
+		/* slower than SolutionThree, due to find in [start, str_len), */
+		/* longer than Three, [start, end) */
+		pos = s.find(s[end], start);
+		if (pos != string::npos && pos < end) {
+			start = pos + 1;
+			cur_len = end - start;
+			// break;  /* break is wrong */
+		}
+		++cur_len;
+		max_len = std::max(cur_len, max_len);
+	}
+	printf("SolutionFour: %d\n", max_len);
+	return max_len;
+}
+
+/* sliding window with search */
+int SolutionFive(const string &s) {
+	int start = 0, end = 0, cur_len = 0, max_len = 0;
+	int str_len = s.length();
+	int pos = 0;
+	/* search [target_b, target_e) in [search_b, search_end) */
+	decltype(s.begin()) search_b;  /* begin of search region */
+	decltype(s.begin()) search_e;  /* end of search region */
+	decltype(s.begin())	target_b;  /* begin of target string */
+	decltype(s.begin()) target_e;  /* end of target string */
+	for (end = 0; end < str_len; ++end) {
+		/* faster than Four, due to searcg in [start, start + len), */
+		/* shorted than  [start, end) */
+		search_b = s.begin() + start;
+		search_e = s.begin() + end;
+		target_b = s.begin() + end;
+		target_e = s.begin() + end + 1;
+		pos = std::search(search_b, search_e, target_b, target_e) - s.begin();
+
+		if (pos < end) {
+			start = pos + 1;
+			cur_len = end - start;
+			// break;  /* break is wrong */
+		}
+		++cur_len;
+		max_len = std::max(cur_len, max_len);
+	}
+
+	printf("SolutionFive: %d\n", max_len);
+	return max_len;
 }
 
 int LengthOfLongestSubstring(const string &s) {
--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #include <string>
+#include <ctime>
+#include <algorithm>
 
 using std::string;
 
@@ -22,15 +24,40 @@ void TestStringAssign() {
 }
 
 void TestStringFind() {
-	string str = "hello world, wordl, hello";
+	string str = "hello world, wordl,he, hello";
+	char *pchar = "hello";
 	std::size_t pos = str.find(str[0], 1);
 	std::size_t pos_unknown = str.find('z', 0);
+	std::size_t pos_h = str.find(pchar, 1, 3);
 	printf("%d\n", pos);
 	printf("%d\n", pos_unknown);
+	printf("%d\n", pos_h);
+}
+
+void TestSearch() {
+	string str = "hello world, wordl,he, hello";
+	auto it = std::search(str.begin(), str.end(), str.begin() + 2, str.begin() + 3 );
+	printf("offset = %d\n", it - str.begin());
+}
+
+
+void TestRand() {
+	srand(time(NULL));
+	int i = 100;
+	int rand_int;
+	while (i--) {
+		int rand_int = rand() % 2 + 1;
+		switch (rand_int) {
+			case 1: printf("%4d: rand = 1\n", 100 - i); break;
+			case 2: printf("%4d: rand = 2\n", 100 - i); break;
+		}
+	}
 }
 
 int main() {
 	// TestASCII();
 	// TestStringAssign();
-	TestStringFind();
+	// TestStringFind();
+	// TestRand();
+	TestSearch();
 }
