2c521a42ec98628b7bc8d308c7d6a17f8fd6c218
DC-Jade <jadebetter8092@gmail.com>
2022-06-13 12:04:18 +0800

    update 00020

--- src/00020_is_valid.cpp
+++ src/00020_is_valid.cpp
@@ -3,19 +3,42 @@
 
 #include <string>
 #include <stack>
+#include <unordered_map>
 
 using std::string;
 using std::stack;
+using std::unordered_map;
 
 bool IsValid(const string &s);
 bool SolutionOne(const string &str);
 bool Solution(const string &str);
 
-bool IsPair(char c1, char c2) {
+bool IsPairOne(const char &c1, const char &c2) {
+	/* c1 - cur char */
+	/* c2 - pop char */
 	if (c1 == ')' && c2 == '(') return true;
 	if (c1 == ']' && c2 == '[') return true;
 	if (c1 == '}' && c2 == '{') return true;
 	return false;
+}
+
+unordered_map<char, char> pairs = {
+	{')', '('},
+	{']', '['},
+	{'}', '{'},
+};
+
+bool IsPairTwo(const char &c1, const char &c2) {
+	/* c1 - cur char */
+	/* c2 - pop char */
+	return pairs[c1] == c2;
+}
+
+bool IsPair(const char &c1, const char &c2) {
+	switch (rand() % 2) {
+		case 1: printf("IsPairTwo\n"); return IsPairTwo(c1, c2);
+		default: printf("IsPairOne\n"); return IsPairOne(c1, c2);
+	}
 }
 
 bool SolutionOne(const string &str) {
